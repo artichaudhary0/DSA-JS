d09ced89a15397b6e57c853153a4ae19a365e14b
Arti chaudhary <chaudhary.aarti1998@gmail.com>
2024-11-25 09:08:20 +0530

    kadane question

---
+++ Kadane/question1.js
@@ -0,0 +1,36 @@
+/* Given an array arr[], 
+the task is to find the subarray that has the maximum sum and return its sum.
+
+Examples:
+
+Input: arr[] = {2, 3, -8, 7, -1, 2, 3}
+Output: 11
+Explanation: The subarray {7, -1, 2, 3} has the largest sum 11.
+
+
+Input: arr[] = {-2, -4}
+Output: –2
+Explanation: The subarray {-2} has the largest sum -2.
+
+
+Input: arr[] = {5, 4, 1, 7, 8}
+Output: 25
+Explanation: The subarray {5, 4, 1, 7, 8} has the largest sum 25.
+*/
+
+function maxSubarraySum(arr) {
+    let res = arr[0];
+
+    for (let i = 0; i < arr.length; i++) {
+        let currSum = 0;
+      
+        for (let j = i; j < arr.length; j++) {
+            currSum = currSum + arr[j];
+            res = Math.max(res, currSum);
+        }
+    }
+    return res;
+}
+
+const arr = [2, 3, -8, 7, -1, 2, 3];
+console.log(maxSubarraySum(arr));
